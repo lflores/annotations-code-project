5407e6281ba6354fe6d5c6086087e23e7517e3fc
Leonardo Flores <flores.leonardo@gmail.com>
2023-11-21 15:49:56 -0300

    chore: Add Slf4j for logs

--- annotations-code-user/src/main/java/org/triadsoft/PersonApp.java
+++ annotations-code-user/src/main/java/org/triadsoft/PersonApp.java
@@ -1,26 +1,21 @@
 package org.triadsoft;
 
-import org.triadsoft.model.AddressImpl;
-import org.triadsoft.model.CustomerImpl;
-import org.triadsoft.model.Person;
-import org.triadsoft.model.PersonImpl;
-
 public class PersonApp {
     public static void main(String[] args) {
 
-        AddressImpl address = new AddressImpl();
-        address.setComments("asdfasdfasf");
-        address.setNumber(1);
+        // AddressImpl address = new AddressImpl();
+        // address.setComments("asdfasdfasf");
+        // address.setNumber(1);
 
-        CustomerImpl customer = new CustomerImpl();
-        customer.setAddress(address);
+        // CustomerImpl customer = new CustomerImpl();
+        // customer.setAddress(address);
 
-        Person person1 = PersonImpl.builder()
-                .firstName("dsfsfdf")
-                .age(18)
-                .lastName("sdfgsdfg")
-                .address(address)
-                .build();
+        // Person person1 = PersonImpl.builder()
+        //         .firstName("dsfsfdf")
+        //         .age(18)
+        //         .lastName("sdfgsdfg")
+        //         .address(address)
+        //         .build();
 
     }
 }
