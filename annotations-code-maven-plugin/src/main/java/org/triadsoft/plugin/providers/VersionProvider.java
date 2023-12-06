37b33db2a1f424f4d748a420778b788aa8418edd
Leonardo Flores <flores.leonardo@gmail.com>
2023-12-06 17:18:50 -0300

    feat: create scafold of project

--- annotations-code-maven-plugin/src/main/java/org/triadsoft/plugin/providers/VersionProvider.java
+++
@@ -1,7 +0,0 @@
-package org.triadsoft.plugin.providers;
-
-import org.apache.maven.plugin.MojoExecutionException;
-
-public interface VersionProvider {
-    String getVersion(String command) throws MojoExecutionException;
-}
