ad8d91b2b7872f5b31addc96459e97d42fd95957
Leonardo Flores <flores.leonardo@gmail.com>
2023-12-06 17:26:49 -0300

    feat: create scafold of project

---
+++ annotations-code-maven-plugin/src/main/java/org/triadsoft/plugin/providers/GitVersionMojo.java
@@ -0,0 +1,31 @@
+package org.triadsoft.plugin.providers;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * An example Maven Mojo that resolves the current project's git revision and
+ * adds that a new {@code exampleVersion}
+ * property to the current Maven project.
+ */
+@Mojo(name = "version", defaultPhase = LifecyclePhase.INITIALIZE)
+public class GitVersionMojo extends AbstractMojo {
+
+    /**
+     * The git command used to retrieve the current commit hash.
+     */
+    @Parameter(property = "git.command", defaultValue = "git rev-parse --short HEAD")
+    private String command;
+
+    @Parameter(property = "project", readonly = true)
+    private MavenProject project;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Executing Mojo");
+    }
+}
